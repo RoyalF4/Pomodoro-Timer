720d13c07dfefb8efd229264df97916d860b94a2
Royal Finch <Royal2791@gmail.com>
2024-05-09 20:16:12 -0400

    added some initial styling
    - added Roboto Mono font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { formatTimerNumber } from './utils';
 import useInterval from './UseInterval';
 
@@ -8,6 +8,9 @@ export default function App() {
   const [timeLeft, setTimeLeft] = useState(START_TIME);
   const [isPaused, setIsPaused] = useState(true);
 
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
   useInterval(
     () => {
       setTimeLeft((timeLeft) => timeLeft - 1);
@@ -16,47 +19,42 @@ export default function App() {
     isPaused
   );
 
-  const minutes = Math.floor(timeLeft / 60);
-  const seconds = timeLeft % 60;
-
-  // useEffect(() => {
-  //   if (seconds === 0 && minutes === 0) {
-  //     setIsPaused(true);
-  //     clearInterval(intervalRef.current);
-  //   }
-  // }, [seconds, minutes]);
-
-  // useEffect(() => {
-  //   if (!isPaused) {
-  //     intervalRef.current = setInterval(() => {
-  //       setTimeLeft((timeLeft) => timeLeft - 1);
-  //     }, 1000);
-  //   }
-
-  //   return () => clearInterval(intervalRef.current);
-  // }, [isPaused, setTimeLeft]);
+  // if timer hits 00:00, pause it
+  useEffect(() => {
+    if (seconds === 0 && minutes === 0) {
+      setIsPaused(true);
+    }
+  }, [seconds, minutes]);
 
   function handleToggleClock() {
-    // if (isPaused && timeLeft === 0) {
-    //   setTimeLeft(START_TIME);
-    // }
+    // if start is clicked and timer is at 0, set timeLeft to max
+    if (isPaused && timeLeft === 0) {
+      setTimeLeft(START_TIME);
+    }
+
     setIsPaused((cur) => !cur);
   }
 
   function handleReset() {
     setIsPaused(true);
-    setTimeLeft(25 * 60);
+    setTimeLeft(START_TIME);
   }
 
   return (
-    <main>
-      <span>
-        {formatTimerNumber(minutes)}:{formatTimerNumber(seconds)}
-      </span>
-      <button onClick={handleToggleClock}>
-        {isPaused ? 'Start' : 'Pause'}
-      </button>
-      <button onClick={handleReset}>Reset</button>
+    <main className="main">
+      <div className="clock">
+        <span className="clock__timer">
+          {formatTimerNumber(minutes)}:{formatTimerNumber(seconds)}
+        </span>
+        <div className="clock__buttons">
+          <button onClick={handleToggleClock} className="clock__toggle">
+            {isPaused ? 'Start' : 'Pause'}
+          </button>
+          <button onClick={handleReset} className="clock__reset">
+            Reset
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
