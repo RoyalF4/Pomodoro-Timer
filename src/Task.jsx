676bef6b94fa199fe79618095b20b1a2bf730cf5
Royal Finch <Royal2791@gmail.com>
2024-05-10 22:03:23 -0400

    feat: :sparkles: create Task component

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,1 +1,13 @@
-export default function Task() {}
+export default function Task() {
+  return (
+    <div className="tasks">
+      <h2>Task</h2>
+      <div className="line-break"></div>
+      <div className="tasks__list"></div>
+      <button className="task__btn-add">
+        <i className="fa-solid fa-plus" style={{ color: '#0008ff' }}></i>Add
+        Task
+      </button>
+    </div>
+  );
+}
